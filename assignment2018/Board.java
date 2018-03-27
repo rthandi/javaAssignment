8f0ec7d675cc39330986d65d0a868e8923c4123d
rthandi <robbie.thandi@gmail.com>
2018-03-27 15:10:47 +0100

    Added board

---
+++ assignment2018/Board.java
@@ -0,0 +1,11 @@
+package assignment2018;
+
+import assignment2018.codeprovided.Display;
+import assignment2018.codeprovided.Pieces;
+
+public class Board implements Display {
+    @Override
+    public void displayBoard(Pieces myPieces) {
+
+    }
+}
