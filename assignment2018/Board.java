1db8720fbc5ad8047f0f9e34c36caba6873a4b26
rthandi <robbie.thandi@gmail.com>
2018-03-28 14:24:02 +0100

    Removed code that I thought I needed but is already provided :(

--- assignment2018/Board.java
+++ assignment2018/Board.java
@@ -13,48 +13,6 @@ public class Board{
 
     public Board(){ }
 
-    public void initialiseBoard() {
-        int x;
-        int y;
-        int colour;
-
-        for (x = 0; x < BOARD_SIZE; x++) {
-            for (y = 0; y < BOARD_SIZE; y++) {
-                if (!(y > 1 && y < 6)) {
-                    if (y < 2) {
-                        //set colour to black
-                        colour = 0;
-                    } else {
-                        //set colour to white
-                        colour = 1;
-                    }
-                    if (y == 1 || y == 6) {
-                        //Pawn
-                        boardArray[x][y] = new Pawn(x, y, colour, this);
-                    } else {
-                        if (x == 0 || x == 7) {
-                            //Rook
-                            boardArray[x][y] = new Rook(x, y, colour, this);
-                        } else if (x == 1 || x == 6) {
-                            //Knight
-                            boardArray[x][y] = new Knight(x, y, colour, this);
-                        } else if (x == 2 || x == 5) {
-                            //Bishop
-                            boardArray[x][y] = new Bishop(x, y, colour, this);
-                        } else if (x == 3) {
-                            //Queen
-                            boardArray[x][y] = new Queen(x, y, colour, this);
-                        } else {
-                            //King
-                            boardArray[x][y] = new King(x, y, colour, this);
-                        }
-
-                    }
-                }
-            }
-        }
-    }
-
     public boolean outOfRange(int x, int y) {
         return x < 0 || x > BOARD_SIZE - 1 || y < 0 || y > BOARD_SIZE - 1;
     }
@@ -75,10 +33,4 @@ public class Board{
 
     //TODO: Method to insert of remove a piece
 
-    public static void main(String[] args) {
-        //TEST
-        Board board = new Board();
-        board.initialiseBoard();
-        System.out.println(board.boardArray);
-    }
 }
---
+++ assignment2018/Move.java
@@ -0,0 +1,6 @@
+package assignment2018;
+
+public class Move {
+
+    private int fromX
+}
