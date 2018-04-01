47cbc74578353e236402bd706e440363d5cd7e3f
rthandi <robbie.thandi@gmail.com>
2018-04-01 21:12:51 +0100

    Moved text display

---
+++ assignment2018/TextDisplay.java
@@ -0,0 +1,11 @@
+package assignment2018;
+
+import assignment2018.codeprovided.Display;
+import assignment2018.codeprovided.Pieces;
+
+public class TextDisplay implements Display {
+    @Override
+    public void displayBoard(Pieces myPieces) {
+
+    }
+}
--- assignment2018/codeprovided/TextDisplay.java
+++
@@ -1,8 +0,0 @@
-package assignment2018.codeprovided;
-
-public class TextDisplay implements Display{
-    @Override
-    public void displayBoard(Pieces myPieces) {
-
-    }
-}
