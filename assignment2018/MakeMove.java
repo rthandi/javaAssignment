a6f3487e4e325dca4081a98177e63ffd55244352
rthandi <robbie.thandi@gmail.com>
2018-05-11 02:29:34 +0100

    fixed a buggy boi

--- assignment2018/MakeMove.java
+++ assignment2018/MakeMove.java
@@ -23,30 +23,32 @@ public class MakeMove {
         if (!(fromX == -1 || fromY == -1 || toX == -1 || toY == -1)) {
             Move inputMove = new Move(player.getBoard().getPiece(fromX, fromY), fromX, fromY, toX, toY, player.getBoard().occupied(toX, toY));
             //Check if move is possible
-            if (player.getBoard().occupied(fromX, fromY) && player.getBoard().getPiece(fromX, fromY).availableMoves().contains(inputMove)) {
-                if (player.getBoard().occupied(toX, toY)) {
-                    //Delete piece from the opponent's piece collection
-                    for (i = 0; i < player.getPieces().getNumPieces(); i++){
-                        Piece currentPiece = player.getOpponent().getPieces().getPiece(i);
-                        Piece pieceToDelete = null;
-                        if (currentPiece.getX() == toX && currentPiece.getY() == toY){
-                            pieceToDelete = currentPiece;
+            if(player.getBoard().getPiece(fromX, fromY).availableMoves() != null){
+                if (player.getBoard().occupied(fromX, fromY) && player.getBoard().getPiece(fromX, fromY).availableMoves().contains(inputMove)) {
+                    if (player.getBoard().occupied(toX, toY)) {
+                        //Delete piece from the opponent's piece collection
+                        for (i = 0; i < player.getPieces().getNumPieces(); i++) {
+                            Piece currentPiece = player.getOpponent().getPieces().getPiece(i);
+                            Piece pieceToDelete = null;
+                            if (currentPiece.getX() == toX && currentPiece.getY() == toY) {
+                                pieceToDelete = currentPiece;
+                            }
+                            if (pieceToDelete != null) {
+                                player.getOpponent().deletePiece(pieceToDelete);
+                                break;
+                            }
                         }
-                        if (pieceToDelete != null){
-                            player.getOpponent().deletePiece(pieceToDelete);
-                            break;
-                        }
+                        //As it is a valid move we know that the piece can be taken if it is occupied
+                        player.getBoard().removePiece(toX, toY);
                     }
-                    //As it is a valid move we know that the piece can be taken if it is occupied
-                    player.getBoard().removePiece(toX, toY);
+                    //Move piece to the position
+                    player.getBoard().setPosition(toX, toY, player.getBoard().getPiece(fromX, fromY));
+                    //Remove piece from old position
+                    player.getBoard().removePiece(fromX, fromY);
+                    //Change the piece's position on the piece's object
+                    player.getBoard().getPiece(toX, toY).setPosition(toX, toY);
+                    return true;
                 }
-                //Move piece to the position
-                player.getBoard().setPosition(toX, toY, player.getBoard().getPiece(fromX, fromY));
-                //Remove piece from old position
-                player.getBoard().removePiece(fromX, fromY);
-                //Change the piece's position on the piece's object
-                player.getBoard().getPiece(toX, toY).setPosition(toX, toY);
-                return true;
             }
         }
         return false;
